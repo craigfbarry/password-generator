2a8154873e4f7d17ab84ccd6c5d8d3ffa5ba62f7
craigfbarry <craigfbarry@gmail.com>
2020-03-01 13:41:16 +1030

    Added variables to function for the inputs, created arrays for numbers and special characters.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var capsRequired = document.getElementById("#capitalChar").value;
+  var specRequired = document.getElementById("#specialChar").value;
+
+  var pwdLength = document.getElementById("#passwordLength").value;
+       
+
+
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
