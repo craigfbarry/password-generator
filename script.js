adb6ba4f4f16d1a888d832b62af2eb26a19b5b8f
Craig Barry <craigfbarry@gmail.com>
2020-03-03 20:28:29 +1030

    Added logic on password length and added for loop.

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
+var passwordString = "";
+var passwordText = ""; 
 var capsRequired = document.querySelector("#capitalChar").checked;
 var specRequired = document.querySelector("#specialChar").checked;
 var pwdLength = document.querySelector("#passwordLength").value;
@@ -16,12 +17,26 @@ var pwdLength = document.querySelector("#passwordLength").value;
 console.log(capsRequired);
 console.log(specRequired);
 console.log(pwdLength);
-console.log("function working");
 
+
+if (pwdLength>7 && pwdLength<129){
+
+    for (i=0;i<pwdLength;i++){
+
+     //   passwordString = (passwordString.concat(passwordText.fromCharCode(97)));
+     passwordString = (passwordText.fromCharCode(65));
+
+
+    }
+    console.log(passwordString);
+}
+
+else {
+    console.log("range invalid");    
+    }
 
 //****notes***
-// smaple code var password=""; initialize before loop
-//            password = password.concat(array??)
+
 
 
 
