d431a1470886d1e66f15220f021e168ce13dbfd3
craigfbarry <craigfbarry@gmail.com>
2020-03-06 19:55:34 +1030

    Added inputs for lower and numeric and the logic for generator. still need validation

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//var password = {
+
+var lowerRequired = document.querySelector("#lowerChar");
+var numRequired = document.querySelector("#numeral");
+var capsRequired = document.querySelector("#capitalChar");
+var specRequired = document.querySelector("#specialChar");
+var pwdLength = document.querySelector("#passwordLength");
+
+//Using an array for special characters as they are not continuous in UTF
+var specialChar =["!","@","#","$","%","^","&","*","(",")"];
 
 
 
-
-
-// Write password to the #password input
+// Write password to the #password input 
 function getRandom(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 function writePassword() {
 var passwordString = "";
-var capsRequired = document.querySelector("#capitalChar").checked;
-var specRequired = document.querySelector("#specialChar").checked;
-var pwdLength = document.querySelector("#passwordLength").value;
 
-//Using an array for special characters as they are not continuous in UTF
-var specialChar =["!","@","#","$","%","^","&","*","(",")"];
-
+console.log(lowerRequired);
+console.log(numRequired);
 
 console.log(capsRequired);
 console.log(specRequired);
-console.log(pwdLength);
+console.log(pwdLength.value);
+
+
 
 
 // Validate password length as requirement is 8-128 characters
 // Use String.fromCharCode() to use UTF (range lower case 97-122)
-if (pwdLength>7 && pwdLength<129){
+if (pwdLength.value>7 && pwdLength.value<129)                      
+    {
 
-    for (i=0;i<pwdLength;i++){
+    for (i=0;i<pwdLength.value;i++){
         var charTypes = getRandom(4);
 
-        if (charTypes ===0){
+        if (charTypes === 0 && lowerRequired.checked === true){
             passwordString = (passwordString.concat(String.fromCharCode(getRandom(26)+97)));
         }
-        else if (charTypes ===1) {
+        else if (charTypes === 1 && numRequired.checked === true) {
             passwordString = (passwordString.concat(getRandom(10)));
         }
-        else if (charTypes ===2 && specRequired === true) {
+        else if (charTypes === 2 && specRequired.checked === true) {
             passwordString = (passwordString.concat(specialChar[getRandom(10)]));
         }
-        else if (charTypes ===3 && capsRequired=== true) {
+        else if (charTypes === 3 && capsRequired.checked === true) {
             passwordString = (passwordString.concat(String.fromCharCode(getRandom(26)+65)));
         }
     
