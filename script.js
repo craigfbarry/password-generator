09e174e67ea6db60d9954dda7bddb26f12f5f3f6
craigfbarry <craigfbarry@gmail.com>
2020-03-06 22:31:16 +1030

    Added validation with a conditional do while loop to ensure one  of each selected is generated

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ var generateBtn = document.querySelector("#generate");
 
 //var password = {
 
-var lowerRequired = document.querySelector("#lowerChar");
-var numRequired = document.querySelector("#numeral");
-var capsRequired = document.querySelector("#capitalChar");
-var specRequired = document.querySelector("#specialChar");
-var pwdLength = document.querySelector("#passwordLength");
+var pwdReq = {
+    lower         :"",
+    number        :"",
+    caps          :"",
+    special       :"",
+    pwdLength     :"",
+   
+    };
+
+pwdReq.lower = document.querySelector("#lowerChar");
+pwdReq.number = document.querySelector("#numeral");
+pwdReq.caps = document.querySelector("#capitalChar");
+pwdReq.special = document.querySelector("#specialChar");
+pwdReq.pwdLength = document.querySelector("#passwordLength");
 
 //Using an array for special characters as they are not continuous in UTF
 var specialChar =["!","@","#","$","%","^","&","*","(",")"];
-
 
 
 // Write password to the #password input 
@@ -20,55 +28,72 @@ function getRandom(max) {
 }
 
 function writePassword() {
-var passwordString = "";
-
-console.log(lowerRequired);
-console.log(numRequired);
-
-console.log(capsRequired);
-console.log(specRequired);
-console.log(pwdLength.value);
-
-
-
 
 // Validate password length as requirement is 8-128 characters
-// Use String.fromCharCode() to use UTF (range lower case 97-122)
-if ((pwdLength.value>7 && pwdLength.value<129)  && 
-(lowerRequired.checked||numRequired.checked||specRequired.checked||capsRequired.checked === true  )         )          
-    
+// And validate at least on character type selected
+
+if (pwdReq.pwdLength.value<8 || pwdReq.pwdLength.value>128)     
     {
+        document.getElementById("password").textContent= "Invalid Range";
+    }
 
-    for (i=0;i<pwdLength.value;i++){
-        var charTypes = getRandom(4);
+else if   (pwdReq.caps.checked === false && pwdReq.lower.checked === false && pwdReq.number.checked === false && pwdReq.special.checked === false)
+    {  
+       document.getElementById("password").textContent= "No Character type selected"; 
+    }
+   
 
-        if (charTypes === 0 && lowerRequired.checked === true){
-            passwordString = (passwordString.concat(String.fromCharCode(getRandom(26)+97)));
+
+else {
+   // Use String.fromCharCode() to use UTF (range lower case 97-122)
+   generatePassword();
+    
+    }
+}
+
+function generatePassword() {
+  var counter = 0; 
+do {
+    var passwordString = "";
+    var validate = [false,false,false,false];
+    for (i=0;i<pwdReq.pwdLength.value;i++){
+        var charType = getRandom(4);
+        
+
+        if (charType === 0 && pwdReq.caps.checked === true) {
+            passwordString += String.fromCharCode(getRandom(26)+65);
+            validate[0] = true;
+                        
         }
-        else if (charTypes === 1 && numRequired.checked === true) {
-            passwordString = (passwordString.concat(getRandom(10)));
+        else if (charType === 1 && pwdReq.lower.checked === true){
+            passwordString += String.fromCharCode(getRandom(26)+97);
+            validate[1] = true;
         }
-        else if (charTypes === 2 && specRequired.checked === true) {
-            passwordString = (passwordString.concat(specialChar[getRandom(10)]));
+        else if (charType === 2 && pwdReq.number.checked === true) {
+            passwordString += getRandom(10);
+            validate[2] = true;
         }
-        else if (charTypes === 3 && capsRequired.checked === true) {
-            passwordString = (passwordString.concat(String.fromCharCode(getRandom(26)+65)));
+        else if (charType === 3 && pwdReq.special.checked === true) {
+            passwordString += specialChar[getRandom(10)];
+            validate[3] = true;
         }
     
 
         else {(i--);}
-         }
+    }
+     counter++
+} while (pwdReq.caps.checked != validate[0] || pwdReq.lower.checked != validate[1] || pwdReq.number.checked != validate[2] || pwdReq.special.checked != validate[3]);
     
+    console.log(counter);
+    console.log(validate);
+    console.log(pwdReq.caps.checked);
+    console.log(pwdReq.lower.checked);
+    console.log(pwdReq.number.checked);
+    console.log(pwdReq.special.checked);
     console.log(passwordString);
     document.getElementById("password").textContent= passwordString;
-    }
-  
-   
+}
 
-else {
-    console.log("range invalid");   
-    document.getElementById("password").textContent= "Invalid input"; 
-    }
 
 //****notes***
 
@@ -80,7 +105,7 @@ else {
 
     //passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
